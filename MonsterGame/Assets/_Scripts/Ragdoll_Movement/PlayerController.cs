8cfbff6d747c32c0d83fa0566df3179934797251
Patrycioss <72610925+Patrycioss@users.noreply.github.com>
2023-05-10 12:20:25 +0200

    Input package added with player controls

--- MonsterGame/Assets/_Scripts/Ragdoll_Movement/PlayerController.cs
+++ MonsterGame/Assets/_Scripts/Ragdoll_Movement/PlayerController.cs
@@ -1,27 +1,61 @@
-using System;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 namespace _Scripts.Ragdoll_Movement
 {
 	[RequireComponent(typeof(Rigidbody))]
 	public class PlayerController : MonoBehaviour
 	{
-		[SerializeField] private float _speed;
-		[SerializeField] private float _strafeSpeed;
-		[SerializeField] private float _jumpForce;
+		[SerializeField] private InputActionAsset _keyMap;
+		
+		[SerializeField] private float _speed = 150;
+		[SerializeField] private float _strafeSpeed = 100;
+		[SerializeField] private float _jumpForce = 0;
 
 		[SerializeField] private bool _isGrounded;
 
 		private Rigidbody _hips;
 
-		private void Start()
+		private InputActionMap _playerActionMap;
+
+		private InputAction _moveForward;
+		private InputAction _moveBackward;
+		private InputAction _moveLeft;
+		private InputAction _moveRight;
+		private InputAction _jump;
+		private InputAction _run;
+
+		private void OnValidate()
+		{
+			if (_keyMap == null) Debug.LogError("Player Controls not assigned!");
+		}
+
+		private void Awake()
 		{
 			_hips = GetComponent<Rigidbody>();
+			_playerActionMap = _keyMap.FindActionMap("Player");
+			
+			if (_playerActionMap == null) Debug.LogError("Input map has no action map called 'Player'");
+			else
+			{
+				_playerActionMap.Enable();
+				
+				_moveForward = _playerActionMap.FindAction("move_forward");
+				_moveBackward = _playerActionMap.FindAction("move_backward");
+				_moveLeft = _playerActionMap.FindAction("move_left");
+				_moveRight = _playerActionMap.FindAction("move_right");
+				_jump = _playerActionMap.FindAction("jump");
+				_run = _playerActionMap.FindAction("run");
+			}
 		}
 
 		private void FixedUpdate()
 		{
-			
+			if (_moveForward.IsPressed())
+			{
+				Debug.Log( "works");
+				_hips.AddForce(_hips.transform.forward * _speed);
+			}
 		}
 	}
 }
