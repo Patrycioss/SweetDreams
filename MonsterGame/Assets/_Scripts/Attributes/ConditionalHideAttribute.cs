e05ca6c975463cc2bb97138a76bd369812599331
Patrycioss <72610925+Patrycioss@users.noreply.github.com>
2023-05-29 14:43:41 +0200

    Added attribute to disable properties in inspector through boolean field

---
+++ MonsterGame/Assets/_Scripts/Attributes/ConditionalHideAttribute.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace _Scripts.Attributes
+{
+	public class ConditionalHideAttribute : PropertyAttribute
+	{
+		public string ConditionalSourceField = "";
+		public bool HideInInspector = false;
+		
+		public ConditionalHideAttribute(string conditionalSourceField)
+		{
+			ConditionalSourceField = conditionalSourceField;
+			HideInInspector = false;
+		}
+
+		public ConditionalHideAttribute(string conditionalSourceField, bool hideInInspector)
+		{
+			ConditionalSourceField = conditionalSourceField;
+			HideInInspector = hideInInspector;
+		}
+	}
+}
---
+++ MonsterGame/Assets/_Scripts/PowerupSpawner.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using _Scripts.Attributes;
+using UnityEngine;
+
+public class PowerupSpawner : MonoBehaviour
+{
+	private void OnDrawGizmos()
+	{
+		
+	}
+}
+
+
