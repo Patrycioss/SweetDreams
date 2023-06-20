96ecee31bb71c6e8775063eabaaac8b4ef5f42c2
Patrycioss <72610925+Patrycioss@users.noreply.github.com>
2023-06-20 11:03:04 +0200

    Fluffo pillows

--- MonsterGame/Assets/_Scripts/Pillow/Pillow.cs
+++ MonsterGame/Assets/_Scripts/Pillow/Pillow.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Net;
 using _Scripts.PlayerScripts;
 using _Scripts.Ragdoll_Movement;
 using UnityEngine;
@@ -19,22 +20,36 @@ namespace _Scripts.Pillow
 
         private void OnCollisionEnter(Collision collision)
         {
+            Debug.Log("Hit");
+            if (collision.collider == this.gameObject) return;
+            
             GameObject obj = collision.collider.gameObject;
+            
 
             Limb hitLimb = obj.GetComponent<Limb>();
             if (hitLimb != null)
             {
                 if (_limb == null) _limb = GetComponent<Limb>();
+                Debug.Log("1");
+
                 Player player = hitLimb.player;
-                if (player == _limb.player || hitLimb.gameObject.Equals(player.pillow1.gameObject) || hitLimb.gameObject.Equals(player.pillow2.gameObject)) return;
+                if (hitLimb.gameObject.Equals(player.pillow1.gameObject) || hitLimb.gameObject.Equals(player.pillow2.gameObject)) return;
+                Debug.Log("2");
+
                 if (player.invincible) return;
                 Player self = _limb.player;
+                Debug.Log("3");
+
                 if (self.sleepiness.tired <= 0) return;
+                Debug.Log("4");
                 if (player.sleepiness.tired <= 0) return;
 
-                Rigidbody rb = _limb.GetComponent<Rigidbody>();
+                Debug.Log("yes yes");
+                
+                Rigidbody rb = this.GetComponent<Rigidbody>();
                 if (rb.velocity.magnitude < 2.5)
                     return;
+                else Debug.Log("Didn't make threshold");
                 
                 GameObject pelvis = player.controller.gameObject;
                 Rigidbody body = pelvis.transform.GetChild(0).GetComponent<Rigidbody>();
